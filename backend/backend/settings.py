5bb8ea3ca8ef000fd5b821c5ec1e6500dde3fd35
dzhelek <hacktues@8e86bfb0e784>
2020-08-23 06:24:26 +0000

    static django files now work on pythonanywhere.com

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from os import environ
+from os import environ, path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR, "static")
+
