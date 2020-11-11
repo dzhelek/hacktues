f31ef61996b0a45e998ff83f7ce55e1c7cc37807
dzhelek <38794411+dzhelek@users.noreply.github.com>
2020-11-11 20:41:38 +0200

    online field is serialized now

--- backend/wave2/serializers.py
+++ backend/wave2/serializers.py
@@ -72,7 +72,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('url', 'is_active', 'first_name', 'last_name', 'email',
                   'technologies', 'form', 'food_preferences', 'tshirt_size',
-                  'alergies', 'username', 'password', 'team_set')
+                  'alergies', 'online', 'username', 'password', 'team_set')
         read_only_fields = 'team_set',
         extra_kwargs = {'password': {'write_only': True}}
 
