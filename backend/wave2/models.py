9c832a0bd4b7a77d65b5cff60504c84d0d988c2d
dzhelek <38794411+dzhelek@users.noreply.github.com>
2020-11-28 17:52:49 +0200

    change id field of model Team to uuid field

--- backend/wave2/models.py
+++ backend/wave2/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
@@ -99,12 +101,13 @@ class Log(models.Model):
     """
     Team actions logger.
     """
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     action = models.JSONField()
     date = models.DateTimeField(auto_now_add=True)
 
 
 class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     users = models.ManyToManyField(User)
 
     name = models.CharField(max_length=100, unique=True)
