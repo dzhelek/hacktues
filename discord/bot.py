29bc2a74b698da4ea7cc906f029c7d15d10e5bd0
dzhelek <38794411+dzhelek@users.noreply.github.com>
2020-12-06 22:09:03 +0200

    add debugging pritns

--- discord/bot.py
+++ discord/bot.py
@@ -22,6 +22,7 @@ async def fetch(client, path='', url=None):
         url = f'{host}/{path}'
 
     async with client.get(url) as response:
+        assert response.status == 200
         return await response.json()
 
 
@@ -32,14 +33,26 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    print(f'member {member.name} joined')
+    print('searching for user...')
     async with aiohttp.ClientSession(auth=auth) as client:
         members_json = await fetch(client, path='users/')
 
+        member_found = False
+        username = f'{member.name}#{member.discriminator}'
         for member_json in members_json:
-            if member_json['username'] == member.name + member.discriminator:
+            if member_json['username'] == username:
+                print('user was found')
+                member_found = True
                 break
 
+        if not member_found:
+            print('no user was found')
+            return
+
+
         if team_set := member_json['team_set']:
+            print('searching for team...')
             team_json = await fetch(client, url=team_set[-1])
 
             team_name = 'team ' + team_json['name']
@@ -48,6 +61,7 @@ async def on_member_join(member):
 
             role = utils.get(guild.roles, name=team_name)
             if role is None:
+                print('creating role and channels...')
                 role = await guild.create_role(reason=reason, name=team_name)
 
                 perms = {
@@ -61,7 +75,10 @@ async def on_member_join(member):
                 )
                 await guild.create_text_channel(team_name, category=category)
                 await guild.create_voice_channel(team_name, category=category)
+            print('assigning role to member...')
             await member.add_roles(role, reason=reason)
+        else:
+            print('member has no team')
 
 
 bot.run(TOKEN)
