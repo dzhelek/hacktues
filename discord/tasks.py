6ef48cce6293e755048c1a4655348ad84fe735ab
Iskren Alexandrov <46263970+Iskrata@users.noreply.github.com>
2022-02-07 01:06:16 +0200

    Add tasks to update users & give roles to existing ones

--- discord/tasks.py
+++ discord/tasks.py
@@ -17,11 +17,13 @@ class Tasks(commands.Cog):
     def cog_unload(self):
         self.fetch_teams.cancel()
 
-    @tasks.loop(minutes=15)
+    @tasks.loop(minutes=3)
     async def fetch_teams(self):
         print('fetching teams...')
+
         async with aiohttp.ClientSession() as client:
-            teams = await request(self.bot, client, path='api/team/get-teams/')['response']
+            teams = await request(self.bot, client, path='api/team/get-teams')
+            teams = teams['response']
 
         # Writes every team as a text message in a separate channel
         await self.all_teams.delete()
@@ -36,7 +38,6 @@ class Tasks(commands.Cog):
                 content=f"{self.all_teams.content}\n{team_name}"
             )
             '''
-            # TODO
             # ? Cycle through every team's members and try
             # ? to find them in discord and set their *team* role
             role = await get_team_role(team_name, self.guild, self.reason)
@@ -46,29 +47,58 @@ class Tasks(commands.Cog):
                 except Exception:
                     continue
                 await member.add_roles(role, reason=self.reason)
-
+        '''
         # ? Cycle through users and sets *captain*, *team* roles
         print('fetching users...')
         async with aiohttp.ClientSession() as client:
-            users = await request(self.bot, client, path='users/')
+            users = await request(self.bot, client, path='api/user/get-discord-users')
+            users = users['response']
 
         for user in users:
+            print("user", user)
+
+            # The owner of the guild
+            if(user['discordId'] == '301127355014053889'):
+                continue
+
             try:
-                member = await self.guild.fetch_member(user['discord_id'])
+                member = await self.guild.fetch_member(user['discordId'])
             except Exception:
                 continue
-            roles = [role for role in member.roles if 'team' in role.name]
 
-            if not user['is_captain']:
+            reason = "Member joined"
+            participant_r = utils.get(member.guild.roles, name='Участник')
+            unapproved_r = utils.get(member.guild.roles, name='Непотвърден')
+            if(user['teamName'] is not None):
+                team_name = 'team ' + user['teamName']
+                team_r = await get_team_role(team_name, member.guild, reason)
+
+            if participant_r not in member.roles:
+                await member.add_roles(participant_r, reason=reason)
+            if unapproved_r in member.roles:
+                await member.remove_roles(unapproved_r, reason=reason)
+            if team_r not in member.roles and (user['teamName'] is not None):
+                await member.add_roles(team_r, reason=reason)
+
+            await member.edit(nick=f"{user['fullName']} {user['studentClass']}")
+
+            roles = [role for role in member.roles if 'team' in role.name]
+            if len(roles) > 0 and user['teamName'] is None:
+                print("Removing roles for", user['fullName'])
+                await member.remove_roles(*roles, reason=self.reason)
+
+            if not user['isCaptain']:
                 await member.remove_roles(self.captain_role,
                                           reason=self.reason)
             else:
                 await member.add_roles(self.captain_role, reason=self.reason)
 
-            if not user['team_set'] or len(roles) > 1:
+            if len(roles) > 1:
                 await member.remove_roles(*roles, reason=self.reason)
-                await member.add_roles(self.captain_role, reason=self.reason)
-                '''
+                team_name = 'team ' + user["teamName"]
+                role = await get_team_role(team_name, member.guild, reason)
+                await member.add_roles(role, reason=self.reason)
+                
         
 
     @fetch_teams.before_loop
